47d11168ed4cc65ce547c711ab23c2597b476017
Alex <holovkooleksii53@gmail.com>
2023-07-13 21:59:40 +0200

    third cart commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,17 +1,10 @@
-let _inc = document.querySelectorAll(".product__quantity-control_inc");
-let inc = Array.from(_inc);
-let _dec = document.querySelectorAll(".product__quantity-control_dec");
-let dec = Array.from(_dec);
-let _amnt = document.querySelectorAll(".product__quantity-value");
-let amnt = Array.from(_amnt);
-let _add = document.querySelectorAll(".product__add");
-let add = Array.from(_add);
-let _product = document.querySelectorAll(".product");
-let product = Array.from(_product);
+let inc = Array.from(document.querySelectorAll(".product__quantity-control_inc"));
+let dec = Array.from(document.querySelectorAll(".product__quantity-control_dec"));
+let amnt = Array.from(document.querySelectorAll(".product__quantity-value"));
+let add = Array.from(document.querySelectorAll(".product__add"));
+let product = Array.from(document.querySelectorAll(".product"));
 let cart = document.querySelector(".cart__products");
-let _imag = document.querySelectorAll(".product__image");
-let imag = Array.from(_imag);
-let arr = [];
+let imag = Array.from(document.querySelectorAll(".product__image"));
 
 for(let i=0; i<inc.length; i++){
     inc[i].addEventListener("click", function () {
@@ -21,7 +14,7 @@ for(let i=0; i<inc.length; i++){
 
 for(let i=0; i<dec.length; i++){
     dec[i].addEventListener("click", function () {
-        if(amnt[i].textContent === "0"){
+        if(parseInt(amnt[i].textContent) === 1){
             return;
         }
         amnt[i].textContent = parseInt(amnt[i].textContent) - 1;
@@ -34,20 +27,11 @@ for(let i=0; i<add.length; i++){
 
         let dataID = parseInt(product[i].getAttribute("data-id"));
 
-        let compare = (elem) => elem === dataID;
+        let compare = (elem) => parseInt(elem.getAttribute("data-id")) === dataID;
 
-        let _cartProduct = document.querySelectorAll(".cart__product");
-        let cartProduct = Array.from(_cartProduct);
+        let cartProduct = Array.from(document.querySelectorAll(".cart__product"));
 
-        let cartFunc = function(){
-            for(let k=0; k<cartProduct.length; k++){
-                arr.push(parseInt(cartProduct[k].getAttribute("data-id")));
-                console.log("this is " + arr);
-            }
-        }
-        cartFunc();
-
-        if(arr.some(compare)){
+        if(cartProduct.some(compare)){
             let current = parseInt(document.querySelector('.cart__product[data-id="' + dataID + '"]').lastChild.textContent);
             document.querySelector('.cart__product[data-id="' + dataID + '"]').lastChild.textContent = current + parseInt(amnt[i].textContent);
         }else{
@@ -55,6 +39,7 @@ for(let i=0; i<add.length; i++){
         }
     });
 }
+
 
 
 
